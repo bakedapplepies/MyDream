e21868cb81c0da4be18c9026325c9a8dc159aaa2
JohnBach <hoangbachtran2016@gmail.com>
2025-02-27 08:51:38 +0700

    Update

--- src/background.js
+++ src/background.js
@@ -1,8 +1,11 @@
-const color_dark1     = [63, 47, 53];
-const color_dark2     = [96, 77, 68];
-const color_dark3     = [120, 121, 99];
-const color_primary   = [146, 177, 162];
-const color_highlight = [197, 236, 253];
+const color_dark1  = [11, 12, 21];
+const color_dark2  = [21, 18, 30];
+const color_dark3  = [36, 27, 40];
+const color_dark4  = [55, 38, 50];
+const color_light1 = [79, 52, 61];
+const color_light2 = [105, 71, 73];
+const color_light3 = [135, 95, 88];
+const color_light4 = [165, 124, 105];
 
 function lerp(a, b, t)
 {
@@ -15,33 +18,32 @@ function rgb_to_string(color)
 }
 
 const backgroundStyle = document.querySelector("body");
-// const parallaxStyle = document.querySelector("div.block")
-// parallaxStyle.style.addEventListener(() => {
-
-// });
 
 window.onload = () => {
     // backgroundStyle.style.backgroundColor = rgb_to_string(color_dark2);
 }
 
 window.addEventListener("scroll", () => {
-    // const [r1, g1, b1] = [209, 79, 62];
-    // const [r2, g2, b2] = [105, 41, 60];
-    const parallax = document.querySelector("div.block")
-    parallax.style.marginTop = `${window.scrollY * -0.3}px`;
-    console.log(parallax.style.marginTop);
-    
-    // const limit = Math.max( document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight) - window.innerHeight; 
-    // console.log(limit)
-    // const currentY = window.scrollY;
-    // const threshold = limit / 3.0;
-    // const percentage = ((Math.max(threshold, currentY) / limit) - (1.0/3.0)) * 1.5;
+    // BACKGROUND PARALLAX EFFECT - DOESN'T WORK YET
+        // const parallax = document.querySelector("div.block")
+        // parallax.style.marginTop = `${window.scrollY * -0.3}px`;
+        // console.log(parallax.style.marginTop);
 
-    // const [r, g, b] = [
-    //     lerp(r1, r2, percentage),
-    //     lerp(g1, g2, percentage),
-    //     lerp(b1, b2, percentage)
-    // ];
-    
-    // backgroundStyle.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+    // BACKGROUND COLOR LERPING
+        // const [r1, g1, b1] = [209, 79, 62];
+        // const [r2, g2, b2] = [105, 41, 60];
+        
+        // const limit = Math.max( document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight) - window.innerHeight; 
+        // console.log(limit)
+        // const currentY = window.scrollY;
+        // const threshold = limit / 3.0;
+        // const percentage = ((Math.max(threshold, currentY) / limit) - (1.0/3.0)) * 1.5;
+
+        // const [r, g, b] = [
+        //     lerp(r1, r2, percentage),
+        //     lerp(g1, g2, percentage),
+        //     lerp(b1, b2, percentage)
+        // ];
+        
+        // backgroundStyle.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
 })
